bb99a4f7e4189ad155ca3a4b013331f06bc19ebb
rynLu76 <43496806+rynLu76@users.noreply.github.com>
2023-08-24 00:05:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-# from st_files_connection import FilesConnection
+from st_files_connection import FilesConnection
 
 conn = st.experimental_connection('s3', type=FilesConnection)
 df = conn.read("my-linkedin-jobs/final_df.csv", input_format="csv", ttl=600)
