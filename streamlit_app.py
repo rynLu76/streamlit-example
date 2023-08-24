e2114f5f272509f8c9bc2b0514ef785094dc4249
rynLu76 <43496806+rynLu76@users.noreply.github.com>
2023-08-24 00:03:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,6 @@ from collections import namedtuple
 import altair as alt
 import math
 import pandas as pd
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
 import streamlit as st
 import os
 from st_files_connection import FilesConnection
