ad3e4d6e208cf1135b435bec6fb37d61a439db04
rynLu76 <43496806+rynLu76@users.noreply.github.com>
2023-08-23 23:16:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@ from collections import namedtuple
 import altair as alt
 import math
 import pandas as pd
-import streamlit as st
-
 """
 # Welcome to Streamlit!
 
@@ -16,23 +14,19 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+import streamlit as st
+from st_files_connection import FilesConnection
+import s3fs
+import os
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+fs = s3fs.S3FileSystem(anon=False)
 
-    points_per_turn = total_points / num_turns
+@st.experimental_memo(ttl=600)
+def read_file(filename):
+    with fs.open(filename) as f:
+        return f.read().decode('utf-8')
+    
+content = read_file("my-linkedin-jobs/final_df.csv")
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+for line in content.strip().split("\n"):
+    print(line)
